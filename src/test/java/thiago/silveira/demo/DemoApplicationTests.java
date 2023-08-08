bb7b380aaca7bff4bbf54e5faefe798e182d4a97
Thiago Mattos <euthiagomattos@gmail.com>
2023-08-08 14:35:41 -0300

    commit 1

---
+++ src/test/java/thiago/silveira/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package thiago.silveira.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
